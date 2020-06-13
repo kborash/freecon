d3d916a1451c0f48a84c28a2f4ab2a6aa5ca018c
Kyle Borash <kyle@borash.com>
2020-06-12 22:12:56 -0800

    Add styling to room creation form.
    Update display of active rooms.

--- lib/freecon_web/live/professor_dashboard.ex
+++ lib/freecon_web/live/professor_dashboard.ex
@@ -14,8 +14,32 @@ defmodule FreeconWeb.ProfessorDashboard do
     {:ok, socket, temporary_assigns: [active_rooms: [], closed_rooms: []]}
   end
 
-  def handle_event("add-room", _, socket) do
-    {:ok, room} = Experiments.create_room(%{professor_id: socket.assigns.professor[:id], name: "Placeholder Name"})
+  def render(assigns) do
+    ~L"""
+    <form phx-submit="add-room">
+      <div class="ml-2 mt-2">
+        <label>
+          <span class="mr-1"></span>
+          <input class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Room name..." type="text" name="room_name" />
+        </label>
+        <input class="bg-blue-500 text-white font-semibold px-4 py-2 rounded" type="submit" value="Add Room" />
+      </div>
+    </form>
+
+    <h1 class="font-bold text-5xl">Active Rooms</h1>
+    <%= for room <- @active_rooms do %>
+      <h2>Name: <%= room.name %>, Code: <%= room.code %> | <a class="hover:underline cursor-pointer" phx-click="deactivate-room" phx-value-room_id="<%= room.id %>">Deactivate</a></h2>
+    <% end %>
+
+    <h1 class="font-bold text-5xl">Inactive Rooms</h1>
+    <%= for room <- @closed_rooms do %>
+      <h2><%= room.code %></h2>
+    <% end %>
+    """
+  end
+
+  def handle_event("add-room", %{"room_name" => room_name}, socket) do
+    {:ok, room} = Experiments.create_room(%{professor_id: socket.assigns.professor[:id], name: room_name})
     socket = assign_rooms(socket)
     {:noreply, socket}
   end
@@ -24,22 +48,6 @@ defmodule FreeconWeb.ProfessorDashboard do
     Experiments.close_room(socket.assigns.professor[:id], room_id)
     socket = assign_rooms(socket)
     {:noreply, socket}
-  end
-
-  def render(assigns) do
-    ~L"""
-    <button phx-click="add-room" class="bg-blue-500 text-white font-semibold px-4 py-2 rounded">Create Room</button>
-
-    <h1 class="font-bold text-5xl">Active Rooms</h1>
-    <%= for room <- @active_rooms do %>
-      <h2><%= room.code %> <button phx-click="deactivate-room" phx-value-room_id="<%= room.id %>">Deactivate</button></h2>
-    <% end %>
-
-    <h1 class="font-bold text-5xl">Inactive Rooms</h1>
-    <%= for room <- @closed_rooms do %>
-      <h2><%= room.code %></h2>
-    <% end %>
-    """
   end
 
   def assign_rooms(socket) do
