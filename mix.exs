034c40bb952055de50c3beebc6a744148cc2fefa
Kyle Borash <kyle@borash.com>
2020-07-02 21:37:07 -0800

    Add timex dependency.

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Freecon.MixProject do
   def application do
     [
       mod: {Freecon.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :timex]
     ]
   end
 
@@ -37,7 +37,7 @@ defmodule Freecon.MixProject do
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_live_view, "~> 0.12.0"},
+      {:phoenix_live_view, "~> 0.13"},
       {:floki, ">= 0.0.0", only: :test},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
@@ -47,7 +47,8 @@ defmodule Freecon.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:argon2_elixir, "~>2.3"}
+      {:argon2_elixir, "~> 2.3"},
+      {:timex, "~> 3.0"}
     ]
   end
 
