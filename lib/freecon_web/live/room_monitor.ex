057016d76d7e3a01f8884f5c90f4f964b94f9e76
Kyle Borash <kyle@borash.com>
2020-09-12 17:50:33 -0800

    Subscribe admin monitor to game updates.

--- lib/freecon_web/live/room_monitor.ex
+++ lib/freecon_web/live/room_monitor.ex
@@ -1,9 +1,30 @@
 defmodule FreeconWeb.RoomMonitor do
   use FreeconWeb, :live_view
 
-  def mount(%{"id" => room_id}, _, socket) do
+  alias Freecon.Rooms
 
-    Phoenix.PubSub.subscribe(Freecon.PubSub, "room_code")
+  def mount(%{"id" => room_id}, _, socket) do
+    room = Rooms.get_room!(room_id)
+
+    socket = assign(
+      socket,
+      room: room,
+      game: GenServer.call(via_tuple(room.code), :game)
+    )
+
+    Phoenix.PubSub.subscribe(Freecon.PubSub, room.code)
     {:ok, socket}
+  end
+
+  def handle_info(:update, socket) do
+    socket = assign(
+      socket,
+      game: GenServer.call(via_tuple(socket.assigns.room.code), :game)
+    )
+    {:noreply, socket}
+  end
+
+  defp via_tuple(name) do
+    {:via, Registry, {Freecon.GameRegistry, name}}
   end
 end
