adbdc079e2b105aed35555304c090e02e4853a5c
Kyle Borash <kyle@borash.com>
2020-10-11 21:55:19 -0800

    Improve styling on professor dashboard screen.

--- lib/freecon_web/live/professor_dashboard.ex
+++ lib/freecon_web/live/professor_dashboard.ex
@@ -17,8 +17,9 @@ defmodule FreeconWeb.ProfessorDashboard do
 
   def render(assigns) do
     ~L"""
+    <div class="w-100 pl-2 pt-2 pb-4">
     <form phx-submit="add-room">
-      <div class="ml-2 mt-2">
+      <div>
         <label>
           <input class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Room name..." type="text" name="room_name" />
         </label>
@@ -28,13 +29,23 @@ defmodule FreeconWeb.ProfessorDashboard do
 
     <h1 class="font-bold text-5xl">Active Rooms</h1>
     <%= for room <- @active_rooms do %>
-      <h2>Name: <%= room.name %>, Code: <a class="hover:underline cursor-pointer" href="<%= Routes.live_path(@socket, FreeconWeb.RoomLive, room.id) %>"><%= room.code %></a> | <a class="hover:underline cursor-pointer" phx-click="deactivate-room" phx-value-room_id="<%= room.id %>">Deactivate</a></h2>
+    <div>
+      <h2 class="text-2xl"><%= room.name %></h2>
+      <div class="pl-2">
+        <a class="hover:underline cursor-pointer" href="<%= Routes.live_path(@socket, FreeconWeb.RoomLive, room.id) %>">Go to <%= room.code %> dashboard -></a><br />
+        <a class="hover:underline cursor-pointer" phx-click="deactivate-room" phx-value-room_id="<%= room.id %>"><button class="bg-gray-500 text-white font-semibold px-2 py-1 rounded">Archive Room</div></a>
+      </div>
+    <div>
     <% end %>
 
     <h1 class="font-bold text-5xl">Inactive Rooms</h1>
     <%= for room <- @closed_rooms do %>
-      <h2><%= room.code %></h2>
+    <h2 class="text-2xl"><%= room.name %></h2>
+    <div class="pl-2">
+      <a class="hover:underline cursor-pointer block" href="<%= Routes.room_review_path(@socket, :show, room.id) %>"><button class="bg-green-500 text-white font-semibold px-2 py-1 rounded">Review room <%= room.code %></button></a>
+    </div>
     <% end %>
+    </div>
     """
   end
 
